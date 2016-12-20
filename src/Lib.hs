f617bc7dd58d8f5941345b5794b81f7a7d8ee2cb
robert.fuszenecker <robert.fuszenecker@dealogic.com>
2016-12-20 10:43:35 +0100

    Documentation header update

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,3 +1,4 @@
+-- |My Demo Module
 module Lib
     ( someFunc
     , isPrime
@@ -7,27 +8,31 @@ module Lib
 import Control.Parallel
 import Control.Parallel.Strategies
 
+-- |Very basic print function (alias)
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
--- |Checks is the argument prime
+-- |Checks if the argument is prime
 isPrime :: Integer -> Bool
 isPrime 0 = False
 isPrime 1 = False
 isPrime 2 = True
 isPrime x = 2 ^ x `mod` x == 2
 
+-- |Checks (in parallel) if the elements in the array are prime
 isPrimesPar :: [Integer] -> [Bool]
 isPrimesPar =
   parMap rseq isPrime
 
--- |Checks is the array elements are prime
+-- |Checks if the elements in the array are prime
 isPrimes :: [Integer] -> [Bool]
 isPrimes =
   map isPrime
 
+-- |Calculates the factorial of the argument
 fact :: (Num b, Enum b) => b -> b
-fact n = foldr (*) 1 $ (\x -> [1..x]) n
+fact n = foldr (*) 1 [1..n]
 
+-- |Calculates the factorial of the argument
 fact' :: Integer -> Integer
-fact' = foldr (*) 1 . (\x -> [1..x])
+fact' n = product [1..n]
