06e6f50b760d419ee968fdbff0b64414b513dba2
Robert Fuszenecker <robert.fuszenecker@betssongroup.com>
2019-10-19 22:35:09 +0200

    Added Euler-32.

--- src/HS2019.hs
+++ src/HS2019.hs
@@ -2,4 +2,37 @@ module HS2019 (
     main
 ) where
 
-main () = "Fons demonstrator huc venit. "
+import Data.List (sort, nub)
+
+-- F# operators
+(|>) x f = f x
+(>>) g f x = f $ g x
+
+ispandigital :: Integer -> Integer -> Bool
+ispandigital x y =
+    (sort $ 
+        (show x) ++
+        (show y) ++
+        (show $ x * y)) 
+    
+    == "123456789"
+
+pandigitalProducts :: () -> [(Integer, Integer, Integer)]
+pandigitalProducts () =
+    [(i, j, i * j) | 
+
+        -- StackOverflow helped me to reduce the search space
+        i <- [1 .. 100], 
+        j <- [(if i > 9 then 123 else 1234) .. (if i > 9 then 987 else 9876)],
+
+        ispandigital i j ]
+
+euler32 :: () -> String
+euler32 () =
+    pandigitalProducts ()
+    |> map (\(x, y, p) -> p)    -- get the product only
+    |> nub                      -- distinct
+    |> sum
+    |> show
+
+main () = euler32 ()
