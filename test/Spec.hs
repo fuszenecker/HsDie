3e65dba15b9644c918b6723189fa920ac54f7d49
Robert Fuszenecker <robert.fuszenecker@betssongroup.com>
2019-12-15 16:15:45 +0100

    Adding tests.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,47 +1,55 @@
+module Main (main, isPrimeTest, permutationsTest) where
+
 import Lib
 import MyMonad
 import Control.Parallel
 import Control.Parallel.Strategies
 
-main :: IO Integer
-main = do
-  putStrLn "Test suite *****"
-  putStrLn $ "IsPrime: " ++ show isPrimeTest
-  putStrLn $ "MyMonad result: " ++ show myMonadTest
-  putStrLn $ "Permutations result: " ++ show permutationsTest
+import Test.Hspec
+--import Test.QuickCheck
+import Control.Exception (evaluate)
 
-  if isPrimeTest && myMonadTest && permutationsTest
-    then return 0
-    else return 1
+main :: IO ()
+main = hspec $ do
+    describe "My Test souite" $ do
+        it "... runs prime test." $ do
+            isPrimeTest `shouldBe` True
+
+        it "... runs monad tests." $ do
+            myMonadTest
+
+        describe "My Test souite" $ do
+            it "... runs permutation tests." $ do
+                permutationsTest
 
 isPrimeTestNumbers = [
-    (0, False),
-    (1, False),
-    (2, True),
-    (3, True),
-    (4, False),
-    (5, True),
-    (6, False),
-    (7, True),
-    (8, False),
-    (9, False),
-    (10, False),
-    (11, True),
-    (12, False),
-    (13, True)
-  ]
+        (0, False),
+        (1, False),
+        (2, True),
+        (3, True),
+        (4, False),
+        (5, True),
+        (6, False),
+        (7, True),
+        (8, False),
+        (9, False),
+        (10, False),
+        (11, True),
+        (12, False),
+        (13, True)
+    ]
 
 isPrimeTest :: Bool
 isPrimeTest = all (\(x, b) -> isPrime x == b) isPrimeTestNumbers
 
 i :: MyMonadType Integer
 i =
-  return 200
+    return 200
 
 myMonadTest =
-  liftMyMonad i == Just 200
+    liftMyMonad i == Just 200
 
 permutationsTest =
-  (length . permutations2) [1,2,3,4,5,6,7] == 5040
+    (length . permutations2) [1,2,3,4,5,6,7] == 5040
 
 -- >>> permutationsTest
