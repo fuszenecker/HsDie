d0a8f352bff8ad63a54a332fedc4f2c8ed4cedc4
Róbert Fuszenecker <robert.fuszenecker@betssongroup.com>
2020-04-10 20:30:19 +0200

    Added tests.

--- library/Die.hs
+++ library/Die.hs
@@ -1,11 +1,73 @@
 -- | An example module.
-module Die (main) where
+module Die where
 
+import System.Environment (getArgs)
 import Main.Utf8 (withUtf8)
+import Text.Read (readMaybe)
 
--- | An example function.
+import Data.Time.Clock
+import Data.Time.Calendar
+import Data.Char
+
+today :: IO (Integer, Int, Int) -- :: (year, month, day)
+today = getCurrentTime >>= return . toGregorian . utctDay
+
+data Date = Date {
+    year :: Int,
+    month :: Int,
+    day :: Int
+} deriving (Eq)
+
+instance Show Date where
+    show x = show (year x) ++ ". " ++ show (month x) ++ ". " ++ show (day x) ++ "."
+
 main :: IO ()
 main = withUtf8 utf8Main
 
+dateToString :: Date -> String
+dateToString (Date y m d) = intToString (((y - 1) * 12 + m - 1) * 31 + d - 1)
+
+stringToDate :: String -> Date
+stringToDate s = Date
+        (index `div` (12 * 31) + 1)
+        ((index `div` 31) `mod` 12 + 1)
+        (index `mod` 31 + 1)
+    where
+        index = foldl (\a x -> a * 26 + ord x - 65) 0 s
+
+intToString :: Int -> String
+intToString 0 = "A"
+intToString j = intToStringV [] j
+    where
+        intToStringV v i =
+            if i > 0 then
+                intToStringV ([(chr $ 65 + i `rem` 26)] ++ v) (i `div` 26)
+            else
+                v
+
+printUsage :: IO ()
+printUsage = putStrLn "die <annus> <mensis> <dies> || die <codicellus> || die"
+
+parseArgs :: [String] -> IO ()
+parseArgs [] = do
+    (y, m, d) <- today
+    let date = Date (fromIntegral y) m d
+    putStrLn $ "Hodie est: " ++ show date
+    parseArgs [show y, show m, show d]
+
+parseArgs [x] = putStrLn $ "Datum: " ++ show (stringToDate x)
+
+parseArgs [_, _] = printUsage
+
+parseArgs [y, m, d] = do
+    let date = Date <$> readMaybe y <*> readMaybe m <*> readMaybe d
+    let code = dateToString <$> date
+
+    case code of
+        Nothing -> putStrLn $ "Convertare datum [" ++ y ++ ", " ++ m ++ ", " ++ d ++ "] non possim."
+        Just c -> putStrLn $ "Codicellum: " ++ c
+
+parseArgs _ = printUsage
+
 utf8Main :: IO ()
-utf8Main = return ()
+utf8Main = getArgs >>= parseArgs
--- test-suite/Main.hs
+++ test-suite/Main.hs
@@ -10,5 +10,32 @@ main = do
 
 spec :: Spec
 spec = parallel $ do
-    it "is trivially true" $ do
-        True `shouldBe` True
+    it "1. 1. 1. is 'A'" $
+        Die.dateToString (Die.Date 1 1 1) `shouldBe` "A"
+
+    it "1. 1. 2. is 'B'" $
+        Die.dateToString (Die.Date 1 1 2) `shouldBe` "B"
+
+    it "1. 1. 3. is 'C'" $
+        Die.dateToString (Die.Date 1 1 3) `shouldBe` "C"
+
+    it "1. 1. 27. is 'BA'" $
+        Die.dateToString (Die.Date 1 1 27) `shouldBe` "BA"
+
+    it "2020. 4. 10. is 'BQTFE'" $
+        Die.dateToString (Die.Date 2020 4 10) `shouldBe` "BQTFE"
+
+    it "'A' is 1. 1. 1." $
+        Die.stringToDate "A" `shouldBe` (Die.Date 1 1 1)
+
+    it "'B' is 1. 1. 2." $
+        Die.stringToDate "B" `shouldBe` (Die.Date 1 1 2)
+
+    it "'C' is 1. 1. 3." $
+        Die.stringToDate "C" `shouldBe` (Die.Date 1 1 3)
+
+    it "'BA' is 1. 1. 27." $
+        Die.stringToDate "BA" `shouldBe` (Die.Date 1 1 27)
+
+    it "'BQTFE' is 2020. 4. 10." $
+        Die.stringToDate "BQTFE" `shouldBe` (Die.Date 2020 4 10)
