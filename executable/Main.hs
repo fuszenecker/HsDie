3d69fda52b8fdbb467840d753e7fa2aeb8ac4280
Róbert Fuszenecker <robert.fuszenecker@betssongroup.com>
2020-05-07 17:39:08 +0200

    Did some separation.

--- executable/Main.hs
+++ executable/Main.hs
@@ -1,4 +1,48 @@
+module Main where
+
+import Data.Time.Calendar
+import Data.Time.Clock
+
+import System.Environment (getArgs)
+import Main.Utf8 (withUtf8)
+import Text.Read (readMaybe)
+
 import qualified Die
 
+-- | UTF-8-safe main method.
 main :: IO ()
-main = Die.main
+main = withUtf8 utf8Main
+
+printUsage :: IO ()
+printUsage = putStrLn "die <annus> <mensis> <dies> || die <codicellus> || die"
+
+dateToCode :: Maybe Die.Date -> IO ()
+dateToCode date = do
+    let code = Die.dateToString <$> date
+
+    case code of
+        Nothing -> putStrLn $ "Convertere datum [" ++ show date ++ "] non possum."
+        Just c ->
+            case c of
+                Right cc -> putStrLn $ "Codicellum: " ++ cc
+                Left e -> putStrLn e
+
+parseArgs :: [String] -> IO ()
+parseArgs [] = do
+    date <- today
+    putStrLn $ "Hodie est: " ++ show date
+    dateToCode $ Just date
+
+    where
+        today = do
+            (y, m, d) <- toGregorian . utctDay <$> getCurrentTime
+            return $ Die.Date (fromIntegral y) m d
+
+parseArgs [x] = putStrLn $ "Datum: " ++ show (Die.stringToDate x)
+parseArgs [_, _] = printUsage
+parseArgs [y, m, d] = dateToCode $ Die.Date <$> readMaybe y <*> readMaybe m <*> readMaybe d
+parseArgs _ = printUsage
+
+-- | Main method.
+utf8Main :: IO ()
+utf8Main = getArgs >>= parseArgs
--- library/Die.hs
+++ library/Die.hs
@@ -1,20 +1,16 @@
 module Die
     (
+        -- Types:
         Date (..),
 
-        main,
+        -- Functions:
         dateToString,
         stringToDate
     ) where
 
-import System.Environment (getArgs)
-import Main.Utf8 (withUtf8)
-import Text.Read (readMaybe)
-
-import Data.Time.Clock
-import Data.Time.Calendar
 import Data.Char
 
+-- | Type to store date.
 data Date = Date {
     year :: Int,
     month :: Int,
@@ -23,10 +19,6 @@ data Date = Date {
 
 instance Show Date where
     show x = show (year x) ++ ". " ++ show (month x) ++ ". " ++ show (day x) ++ "."
-
--- | Main method.
-main :: IO ()
-main = withUtf8 utf8Main
 
 -- | Converts a date to a string. If any of the year/month/day is non-positive, the result will be an error.
 dateToString :: Date -> Either String String
@@ -50,36 +42,3 @@ stringToDate s = Date
     where
         cleanString = filter isAlpha . map toUpper
         index = foldl (\a x -> a * 26 + ord x - 65) 0 $ cleanString s
-
-printUsage :: IO ()
-printUsage = putStrLn "die <annus> <mensis> <dies> || die <codicellus> || die"
-
-dateToCode :: Maybe Date -> IO ()
-dateToCode date = do
-    let code = dateToString <$> date
-
-    case code of
-        Nothing -> putStrLn $ "Convertere datum [" ++ show date ++ "] non possum."
-        Just c ->
-            case c of
-                Right cc -> putStrLn $ "Codicellum: " ++ cc
-                Left e -> putStrLn e
-
-parseArgs :: [String] -> IO ()
-parseArgs [] = do
-    date <- today
-    putStrLn $ "Hodie est: " ++ show date
-    dateToCode $ Just date
-
-    where
-        today = do
-            (y, m, d) <- toGregorian . utctDay <$> getCurrentTime
-            return $ Date (fromIntegral y) m d
-
-parseArgs [x] = putStrLn $ "Datum: " ++ show (stringToDate x)
-parseArgs [_, _] = printUsage
-parseArgs [y, m, d] = dateToCode $ Date <$> readMaybe y <*> readMaybe m <*> readMaybe d
-parseArgs _ = printUsage
-
-utf8Main :: IO ()
-utf8Main = getArgs >>= parseArgs
