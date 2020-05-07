fbfae70b4c8cd138d5d50044938ba3b14df0dfc0
Róbert FUSZENECKER <fuszenecker@users.noreply.github.com>
2020-05-07 18:11:47 +0200

    Create update-apt-repository.sh

---
+++ update-apt-repository.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+rm -rf debs
+mkdir debs
+find -L ~/dev | egrep  "\.deb$" | xargs -I _ cp _ debs
+
+dpkg-sig -k F20 --sign fuszenecker debs/*.deb
+
+dpkg-scanpackages debs > Packages
+gzip -fk9 Packages
+
+apt-ftparchive release . > Release
+
+rm -fr InRelease
+gpg --default-key F20 --clearsign -o InRelease Release
+
+# deb [trusted=yes] file:/home/fuszenecker/packages ./
